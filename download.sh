60d4446ccb0658f478f3abdcaadeb677ed4fd333
Dio <gank23456@gmail.com>
2022-10-09 10:45:15 +0800

    final V1

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 # -O 指定名稱 -P指定目錄
-wget https://www.dropbox.com/s/uxe1ebkers86072/intent_model.ckpt?dl=1 -O intent_model.ckpt
-wget https://www.dropbox.com/s/l9kq3obyyik4sz1/slot_model.ckpt?dl=1 -O slot_model.ckpt
+wget https://www.dropbox.com/s/kfrq0onvfqp5m5y/intent_model.ckpt?dl=1 -O intent_model.ckpt
+wget https://www.dropbox.com/s/mj6m90oqhc29dgc/slot_model.ckpt?dl=1 -O slot_model.ckpt
